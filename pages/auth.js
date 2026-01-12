bf307ce7ca368afb2cac68b314f2a7a08bced813
Triple twirl <himanshuanand861@gmail.com>
2026-01-13 02:58:38 +0530

    Update import paths in auth.js
    fix relative import paths

--- pages/auth.js
+++ pages/auth.js
@@ -1,5 +1,5 @@
-import { connectDB } from "../../lib/db";
-import UserOTP from "../../models/UserOTP";
+import { connectDB } from "../lib/db";
+import UserOTP from "../models/UserOTP";
 
 export default async function handler(req,res){
   await connectDB();

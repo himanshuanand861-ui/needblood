b690fd42d962aa4a30289eb9064d3ad68ba1a23a
Triple twirl <himanshuanand861@gmail.com>
2026-01-13 02:40:37 +0530

    Delete app/app directory
    Remove app router folder

--- app/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", fontSize: "24px" }}>
-      <h1>Need Blood Project Started ❤️</h1>
-      <p>My Next.js project is running successfully.</p>
-    </div>
-  );
-}
